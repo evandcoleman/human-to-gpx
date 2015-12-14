817fc6390f6757ff44d2414e5bca2799aa5031c8
Evan Coleman <e@edc.me>
2015-12-13 22:13:20 -0500

    Use submodules instead of cocoapods cause swift

--- Pods/Target Support Files/Swiftline/Swiftline-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Swiftline : NSObject
-@end
-@implementation PodsDummy_Swiftline
-@end
--- human-to-gpx/main.swift
+++ human-to-gpx/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+let opts = Args.parsed.flags
+
+let directory = opts["d"]
+let file = opts["f"]
+
+if directory == nil && file == nil {
+    print("Usage:\n".f.Blue)
+    print("  Specify a Human JSON file: -f <path>\n".f.Blue)
+    print("  Specify a Human archive folder: -d <path>\n".f.Yellow)
+    
+    exit(0)
+} else if directory != nil && file != nil {
+    print("You can only specify an archive directory or file path. Not both.".f.Red)
+    
+    exit(0)
+}
 
