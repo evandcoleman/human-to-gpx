9c9fd249d749b25d769fc403ff5fa420ac5cef6c
Evan Coleman <e@edc.me>
2015-12-13 21:17:41 -0500

    Initial Commit

---
+++ human-to-gpx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  human-to-gpx
+//
+//  Created by Evan Coleman on 12/13/15.
+//  Copyright © 2015 Evan Coleman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
